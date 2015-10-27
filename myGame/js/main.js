51fe701e97e13815224b0d2e8055137119a84bc0
Sonia <sonia.pepe@fdvsolutions.com>
2015-10-27 16:07:57 -0300

    Creating my cards game, and showing cards on display with a for loop.

---
+++ myGame/js/main.js
@@ -0,0 +1,1 @@
+var jokerWin = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
---
+++ myGame/js/preload.js
@@ -0,0 +1,4 @@
+function preload() {
+	jokerWin.load.image('green-background', 'assets/green-bg.png');
+	jokerWin.load.spritesheet('cards', 'assets/cards-sprite.png', 81, 117, 65);
+}
