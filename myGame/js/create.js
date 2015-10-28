060b47117e335f5bb978ec1015c12783d31f3d79
Sonia <sonia.pepe@fdvsolutions.com>
2015-10-28 10:53:50 -0300

    Making the code clearer.

--- myGame/js/create.js
+++ myGame/js/create.js
@@ -1,11 +1,17 @@
 function create() {
 
 	jokerWin.cardsArray = [];
+	
 	for (var verticalIndex = 0; verticalIndex < 4; verticalIndex++) {
 
 		for (var horizontalIndex = 0; horizontalIndex < 13; horizontalIndex++) {
 			console.log('verticalIndex: '+ verticalIndex, 'horizontalIndex: '+ horizontalIndex);
-			var cardSprite = jokerWin.add.sprite(horizontalIndex*60, verticalIndex*90, 'cards',(verticalIndex*13)+horizontalIndex);
+
+			var cardSprite = jokerWin.add.sprite(horizontalIndex*60, 
+							 verticalIndex*90, 
+							 'cards',
+							 (verticalIndex*13)+horizontalIndex);
+
 			cardSprite.scale.set(0.7)
 			jokerWin.cardsArray.push({
 				sprite: cardSprite,
