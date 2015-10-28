f12ace0b6f4ce0d97600bb54b27924af0e38b93c
Sonia <sonia.pepe@fdvsolutions.com>
2015-10-28 17:51:08 -0300

    Playing with some functions, don´t know what am I doing.

--- myGame/js/create.js
+++ myGame/js/create.js
@@ -1,18 +1,22 @@
 function create() {
 
+	jokerWin.add.sprite(-40, -40, 'green-background');
+
 	jokerWin.cardsArray = [];
-	
+
 	for (var verticalIndex = 0; verticalIndex < 4; verticalIndex++) {
 
 		for (var horizontalIndex = 0; horizontalIndex < 13; horizontalIndex++) {
-			console.log('verticalIndex: '+ verticalIndex, 'horizontalIndex: '+ horizontalIndex);
 
-			var cardSprite = jokerWin.add.sprite(horizontalIndex*60, 
-							 verticalIndex*90, 
+			/*console.log('verticalIndex: '+ verticalIndex, 'horizontalIndex: '+ horizontalIndex);*/
+
+			var cardSprite = jokerWin.add.sprite(horizontalIndex*75, 
+							 verticalIndex*100, 
 							 'cards',
 							 (verticalIndex*13)+horizontalIndex);
 
-			cardSprite.scale.set(0.7)
+			cardSprite.scale.set(0.7);
+
 			jokerWin.cardsArray.push({
 				sprite: cardSprite,
 				cardNumber: horizontalIndex+2,
@@ -20,6 +24,13 @@ function create() {
 			});
 		};
 	};	
+
+	var cards = jokerWin.add.sprite(420, 400, 'cards');
+
+	cards.scale.set(1.3);
+
+	cards.animations.add('jump');
+	cards.animations.play('jump', 2, true);
 	
 }
 
--- myGame/js/preload.js
+++ myGame/js/preload.js
@@ -1,4 +1,4 @@
 function preload() {
-	jokerWin.load.image('green-background', 'assets/green-bg.png');
+	jokerWin.load.image('green-background', 'assets/green-bg.jpg');
 	jokerWin.load.spritesheet('cards', 'assets/cards-sprite.png', 81, 117, 65);
 }
