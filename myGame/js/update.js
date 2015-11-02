7372bf803549b00ce39fc0c194b77b3d54428a42
Sonia <sonia.pepe@fdvsolutions.com>
2015-11-02 11:00:30 -0300

    Making the player able to play with the arrow keys

--- myGame/js/update.js
+++ myGame/js/update.js
@@ -1,4 +1,39 @@
 function update() {
 //  Collide the player and the stars with the platforms
 	jokerWin.physics.arcade.collide(dude, platforms);
+
+	var cursors = jokerWin.input.keyboard.createCursorKeys() ;
+
+
+	//  Reset the players velocity (movement)
+    dude.body.velocity.x = 0;
+
+    if (cursors.left.isDown) {
+    
+        //  Move to the left
+        dude.body.velocity.x = -150;
+        dude.animations.play('left');
+    }   
+    
+    else if (cursors.right.isDown) {
+    
+        //  Move to the right
+        dude.body.velocity.x = 150;
+        dude.animations.play('right');
+        
+    }    
+    
+    else {
+        //  Stand still
+        dude.animations.stop()
+        dude.frame = 4;
+    }    
+
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && dude.body.touching.down) {
+        dude.velocity.y = -350;
+    };
+    
+        
+    
 }
