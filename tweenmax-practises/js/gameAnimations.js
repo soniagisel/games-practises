d9884d8abccb715b92cb7780d608a774a64a7a0a
Sonia <sonia.pepe@fdvsolutions.com>
2015-11-12 14:31:20 -0300

    Adding new effects

--- tweenmax-practises/js/gameAnimations.js
+++ tweenmax-practises/js/gameAnimations.js
@@ -7,10 +7,25 @@ $(document).ready(function(){
 
     TweenMax.from($('.balloons1'), 1, {
     	delay: 1,
-    	scale: 0
+    	scale: 0,
+    	rotationX:45, 
+    	scaleX:0.8, 
+    	z:-300
+    });
+
+    TweenMax.from($('.btn-spin'), 1, {
+    	delay: 1.5,
+    	top: -600,
+    	ease: Power2.easeOut
     });
 
     TweenMax.from($('.balloons2'), 1, {
+    	delay: 2,
+    	scale: 0
+    });
+
+
+    TweenMax.from($('.wheel'), 1, {
     	delay: 2,
     	scale: 0
     });
@@ -20,17 +35,18 @@ $(document).ready(function(){
     	scale: 100
     });
 
-    TweenMax.from($('.btn-spin'), 1, {
-    	delay: 1.5,
-    	top: -600,
-    	ease: Power2.easeOut
+    TweenMax.to($('.balloons2'), 2, {
+    	rotation:"1.25rad", 
+    	skewX:"30deg",
+    	yoyo: true,
+    	repeat: -1
     });
 
     $('.btn-spin').on('click', function(){
     	$('.btn-spin-down').css('display', 'block');
 
-    	TweenMax.from($('.wheel'), 15, {
-    		rotation: 360*30,
+    	TweenMax.to($('.wheel'), 15, {
+    		rotation: 360*30 - 120,
     		ease: Power1.easeInOut
     	});
     });
