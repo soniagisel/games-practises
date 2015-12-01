6df279dceb3d30d60a7645d2f9266848274cc39b
Sonia <sonia.pepe@fdvsolutions.com>
2015-12-01 15:49:49 -0300

    Made the racquets stop if keys are up.

--- tenis/js/update.js
+++ tenis/js/update.js
@@ -1,13 +1,17 @@
 function update () {
+	//Arrow keys
 	cursors = tennis.input.keyboard.createCursorKeys();
 
 	if (cursors.left.isDown) {
 		racquet_1.body.velocity.x = -300 ;
 	} else if (cursors.right.isDown) {
 		racquet_1.body.velocity.x = 300 ;
-	} 
+	} else if (cursors.left.isUp || cursors.right.isUp){
+		racquet_1.body.velocity.x = 0;
+	}
 
 
+	//A and D keys
 	left = tennis.input.keyboard.addKey(Phaser.Keyboard.A);
     right = tennis.input.keyboard.addKey(Phaser.Keyboard.D);
 
@@ -15,7 +19,10 @@ function update () {
 		racquet_2.body.velocity.x = -300 ;
 	} else if (right.isDown) {
 		racquet_2.body.velocity.x = 300 ;
+	} else if (left.isUp || right.isUp){
+		racquet_2.body.velocity.x = 0;
 	}
+
 
 	//tennis.physics.arcade.collide(racquets, ball);
 };
